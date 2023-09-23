46f322ce1403da0c010ad8e5c3451fa7389da613
laterNaT <105680553+laterNaT@users.noreply.github.com>
2023-09-23 15:19:22 +0200

    Change footer URL based on path

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,14 +1,19 @@
 "use client";
 
 import { Flex, Text } from "@chakra-ui/react";
+import { usePathname } from "next/navigation";
 
 export default function Footer() {
+  const pathname = usePathname();
+  const url = pathname === "/" ? "/about" : "/";
+  const urlText = url === "/" ? "Home" : "About us";
+
   return (
     <Flex w="100" h="50" bg="black" justifyContent="center" alignItems="center">
       <Text color="white">
         TangoVault -{" "}
-        <Text as="a" href="#" color="purple.300">
-          About us
+        <Text as="a" href={url} color="purple.300">
+          {urlText}
         </Text>
       </Text>
     </Flex>

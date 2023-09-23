f4420b060e46190394fb610fcce7ef8462e7047f
laterNaT <105680553+laterNaT@users.noreply.github.com>
2023-09-23 17:00:03 +0200

    Display logged in user's name

--- app/(secure)/page.tsx
+++ app/(secure)/page.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import { Box, Button, Center, Heading, VStack } from "@chakra-ui/react";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession();
+
   return (
     <Box h="100%" mt="28">
       <Center>
-        <Heading>Welcome back, X.</Heading>
+        <Heading>Welcome back, {session?.user?.name}.</Heading>
       </Center>
       <VStack spacing={5} mt="20">
         <Button size="lg" bg="brand.500" minW="15rem">

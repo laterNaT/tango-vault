381beb5474d5c633ee09c5919c25bb488d7fb203
laterNaT <105680553+laterNaT@users.noreply.github.com>
2023-10-28 16:40:10 +0200

    Update error handling for /api/auth/register

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,18 +1,20 @@
 import { registerUser } from "@/app/_helpers/server/users-repo";
-import { redirect } from "next/navigation";
 import { NextRequest } from "next/server";
 
 async function register(request: NextRequest) {
   const { username, password } = await request.json();
 
+  if (!password || !password) {
+    return new Response("Missing username or password", {status: 400})
+  }
+
   try {
-    await registerUser({ username, password });
-  } catch (err) {
+    await registerUser({username, password});
+  } catch (err: unknown) {
     if (err instanceof Error) {
       if (err.message === "Username already exists") {
         return new Response(err.message, { status: 409 });
       }
-      return new Response(err.message, { status: 500 });
     }
     return new Response("Internal server error", { status: 500 });
   }

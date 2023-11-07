4bdda9ede1b2431d9d32d5cc12e51a0316e0014c
laterNaT <105680553+laterNaT@users.noreply.github.com>
2023-11-07 17:19:37 +0100

    Refactor route handler
    No middleware for now, perhaps add in future if multiple middlwares required.

--- app/api/collections/route.ts
+++ app/api/collections/route.ts
@@ -1,28 +1,26 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
 import { OPTIONS } from "@/app/_config/options";
 import { newCollection as newCollectionDB } from "@/app/_helpers/server/userdata-repo";
 
 async function newCollection(req: Request, res: NextApiResponse) {
-  const session = await getServerSession(OPTIONS);
-  if (!session) {
-    // TODO: add middleware for securing all API routes
-    return new Response("session error", { status: 401 });
+  try {
+    const session = await getServerSession(OPTIONS);
+
+    if (!session || !session.user) {
+      return new Response("Session error", { status: 401 });
+    }
+
+    const data = await req.json();
+    const id = session.user.id;
+
+    await newCollectionDB({ id, ...data });
+
+    return new Response("New collection created successfully", { status: 200 });
+  } catch (e) {
+    console.log(e);
+    return new Response("Internal server error", { status: 500 });
   }
-
-  const data = await req.json();
-  const id = session?.user?.id;
-
-  if (!id) {
-    return new Response("session error", { status: 401 }); // todo: fix session type
-  }
-
-  const params = { id, ...data };
-
-  // here we will call database function to actually save stuff, yeh
-  await newCollectionDB(params);
-
-  return new Response("hello", { status: 200 });
 }
 
 export { newCollection as POST };
